a6cece104e8facc775590b134dbb9d3d404e13ed
hun-jae <tint1998@gmail.com>
2021-11-16 13:44:57 +0900

    camera rotation by mouse

--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -4,24 +4,68 @@ using UnityEngine;
 
 public class PlayerMove : MonoBehaviour
 {
-    public float speed;
-    float hAxis;
-    float vAxis;
-    Vector3 moveVec;
+    [SerializeField]
+    private float walkSpeed;
 
+    [SerializeField]
+    private float lookSensitivity;
+
+
+    [SerializeField]
+    private float cameraRotationLimit;
+    private float currentCameraRotationX = 0;
+
+
+    [SerializeField]
+    private Camera theCamera;
+
+    private Rigidbody myRigid;
+
+
+
+    private void Start()
+    {
+
+        myRigid = GetComponent<Rigidbody>();
+    }
 
 
     // Update is called once per frame
     void Update()
     {
-        hAxis = Input.GetAxisRaw("Horizontal");
-        vAxis = Input.GetAxisRaw("Vertical");
+        Move();
+        CameraRotation();
+        CharacterRotation();
+    }
 
-        moveVec = new Vector3(hAxis, 0, vAxis).normalized;
-        transform.position += moveVec * speed * Time.deltaTime;
+    private void Move()
+    {
+        float _moveDirX = Input.GetAxisRaw("Horizontal");
+        float _moveDirZ = Input.GetAxisRaw("Vertical");
 
+        Vector3 _moveHorizontal = transform.right * _moveDirX;
+        Vector3 _moveVertical = transform.forward * _moveDirZ;
 
+        Vector3 _velocity = (_moveHorizontal + _moveVertical).normalized * walkSpeed; //대각선 이동속도도 동일하게 맞춰주기 위해 normalized를 통해 정규화
 
-        transform.LookAt(transform.position + moveVec); //이후 마우스에 따라 시점 변경
+        myRigid.MovePosition(transform.position + _velocity * Time.deltaTime);
+
+    }
+
+    private void CharacterRotation() //좌우 캐릭터 회전
+    {
+        float _yRotation = Input.GetAxisRaw("Mouse X");
+        Vector3 _characterRotationY = new Vector3(0f, _yRotation, 0f) * lookSensitivity;
+        myRigid.MoveRotation(myRigid.rotation * Quaternion.Euler(_characterRotationY));
+    }
+
+    private void CameraRotation() //상하 카메라 회전
+    {
+        float _xRotation = Input.GetAxisRaw("Mouse Y");
+        float _cameraRotationX = _xRotation * lookSensitivity;
+        currentCameraRotationX -= _cameraRotationX;
+        currentCameraRotationX = Mathf.Clamp(currentCameraRotationX, -cameraRotationLimit, cameraRotationLimit);
+
+        theCamera.transform.localEulerAngles = new Vector3(currentCameraRotationX, 0f, 0f);
     }
 }
