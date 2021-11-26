6f8151c752d72938cdbce8d0d6cc908bf5f45563
hun-jae <tint1998@gmail.com>
2021-11-26 17:46:53 +0900

    edit item, UI

--- Assets/Scripts/Item.cs
+++ Assets/Scripts/Item.cs
@@ -22,9 +22,9 @@ public class Item : MonoBehaviour
     {
         Vector3 item_location;
         if (type == Type.Heart)
-            item_location = new Vector3(2f, 25f, -225f); //추후 random, 3f, random으로 변경 random값은 -225 + (0~9)*50 혹은 스폰지역을 정해두고 하자
+            item_location = new Vector3(2f, 50f, -225f); //추후 random, 3f, random으로 변경 random값은 -225 + (0~9)*50 혹은 스폰지역을 정해두고 하자
         else
-            item_location = new Vector3(-8f, 25f, -225f); //추후 random, 3f, random으로 변경 random값은 -225 + (0~9)*50 혹은 스폰지역을 정해두고 하자
+            item_location = new Vector3(-8f, 50f, -225f); //추후 random, 3f, random으로 변경 random값은 -225 + (0~9)*50 혹은 스폰지역을 정해두고 하자
         transform.position = item_location;
     }
 }
